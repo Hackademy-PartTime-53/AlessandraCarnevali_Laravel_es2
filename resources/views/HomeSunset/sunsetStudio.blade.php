19f5eff5f3f3937d3f0dae842716e50354994552
Alessandra Carnevali <alessandracarnevali@AirdiAlessandra.homenet.telecomitalia.it>
2025-03-17 13:46:55 -0700

    Modifiche

--- resources/views/HomeSunset/sunsetStudio.blade.php
+++ resources/views/HomeSunset/sunsetStudio.blade.php
@@ -32,7 +32,7 @@
                             </li>
                             <li class="nav-item dropdown">
                                 <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Details
+                                    Extra
                                 </a>
                                 <ul class="dropdown-menu">
                                     <li><a class="dropdown-item" href="#">Action</a></li>
